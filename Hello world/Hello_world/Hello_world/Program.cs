2a29af128301cb74553bc4f7c90b9ea3c294ce93
congtanh <congtanhtc@gmail.com>
2021-10-08 17:22:40 +0700

    My first program - Hello world

---
+++ Hello world/Hello_world/Hello_world/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Hello_world
+{
+    class Program
+    {
+        /// <summary>
+        /// 
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
+
